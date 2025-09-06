e3b103a16dacd97d64f10a0614553c3274a0dd43
daniel <king_all_in_hills@protonmail.com>
2025-09-06 11:47:43 +0530

    add search by date

--- src/imgbatch/utils/find_files.py
+++ src/imgbatch/utils/find_files.py
@@ -1,10 +1,25 @@
 import glob
 import os
+from datetime import datetime
 
 
-def find_files(ext: str, directory: str, recurse_folder: bool = False) -> list:
+def find_files(
+    ext: str,
+    directory: str,
+    recurse_folder: bool = False,
+    date: str = None,
+) -> list:
     pattern = f"**/*.{ext}" if recurse_folder else f"*.{ext}"
+    if date is not None:
+        list_of_files = [
+            file
+            for file in glob.glob(os.path.join(directory, pattern))
+            if date == datetime.fromtimestamp(os.path.getmtime(file)).date()
+        ]
+        return list_of_files
+
     files = glob.glob(os.path.join(directory, pattern))
+
     if not files:
         raise FileNotFoundError(f"No '.{ext}' files found in {directory}")
     return files
